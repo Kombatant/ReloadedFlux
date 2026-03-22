ec3dd7ed309f1c4f9d3cceae3c728781aa2c750e
Pete Vagiakos <github@kombatant.com>
2026-03-22 10:49:38 -0400

    fix: Smooth scrolling adjustment

--- src/hooks/useKeyHandlers.js
+++ src/hooks/useKeyHandlers.js
@@ -200,7 +200,7 @@ const useKeyHandlers = () => {
       focusStreamCard(selectedCard)
 
       if (!hasAppliedInitialScroll) {
-        scrollStreamCardIntoView(selectedCard, scrollElement)
+        scrollStreamCardIntoView(selectedCard, scrollElement, "smooth")
         hasAppliedInitialScroll = true
         stableFrameCount = 0
         streamAlignmentTaskRef.current.delayTimeoutId = globalThis.setTimeout(() => {
